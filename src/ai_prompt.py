65cbb9a31c1b749501b7c556e1d15299c7ebca89
ghilles23 <114549984+ghilles23@users.noreply.github.com>
2025-01-21 10:36:57 -0500

    Initial program

---
+++ src/ai_prompt.py
@@ -0,0 +1,15 @@
+from pipeline_QA import ask_question
+
+if __name__ == "__main__":
+    print("Welcome to Diorite")
+    context = input("Enter a question: ")
+
+    while True:
+        question = input("\Ask Question:")
+        if question.lower() == 'exit'
+            print("Goodbye")
+            break
+        answer = ask_question(question, context)
+        print(f"Answer: {answer}")
+        
+
